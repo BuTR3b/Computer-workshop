1c0ea1ffe31a52f50eac0220c4b0c586adb42bb5
BuTR3b <43819843+BuTR3b@users.noreply.github.com>
2020-04-18 17:39:59 +0300

    Add files via upload

---
+++ Str/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace CP_str
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Task 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(622, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 150);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Task 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(206, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(241, 41);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(241, 67);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(39, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Task 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 68);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(206, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(241, 166);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(39, 20);
+            this.textBox7.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(300, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Task 6";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(13, 167);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(206, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(225, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Task 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(13, 141);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(206, 20);
+            this.textBox10.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(225, 111);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Task 4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(13, 112);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(206, 20);
+            this.textBox11.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(225, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Task 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(13, 206);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(206, 20);
+            this.textBox9.TabIndex = 17;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(225, 245);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "Task 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(13, 246);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(206, 20);
+            this.textBox12.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 21;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(225, 280);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 23;
+            this.button9.Text = "Task 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(13, 281);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(206, 20);
+            this.textBox13.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox13;
+    }
+}
+
---
+++ Str/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CP_str
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox1.Text;
+            string new_str = "";
+            for (int i = 0; i < my_str.Length; i++)
+            {
+                if (my_str[i] != '*')
+                {
+                    new_str += my_str[i];
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox3.Text;
+            string new_str = "";
+            char to_del = Convert.ToChar(textBox4.Text);
+            for (int i = 0; i < my_str.Length; i++)
+            {
+                if (my_str[i] != to_del)
+                {
+                    new_str += my_str[i];
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox6.Text + ' ';
+            string new_str = "";
+            string to_del = textBox5.Text;
+            for (int i = 0; i < my_str.Length - 1; i++)
+            {
+                if (my_str[i] == to_del[0] && my_str[i + 1] == to_del[1])
+                {
+                    i++;
+                }
+                else 
+                {
+                    new_str += my_str[i];
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox11.Text;
+            string new_str = "";
+            for (int i = 0; i < my_str.Length; i++)
+            {
+                if (my_str[i] != '*')
+                {
+                    new_str += my_str[i];
+                }
+                else
+                {
+                    new_str += '?';
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox10.Text;
+            string new_str = "";
+            for (int i = 0; i < my_str.Length; i++)
+            {
+                if (my_str[i] != '5')
+                {
+                    new_str += my_str[i];
+                }
+                else
+                {
+                    new_str += "пять";
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox8.Text + ' ';
+            string new_str = "";
+            string to_del = textBox7.Text;
+            for (int i = 0; i < my_str.Length - 1; i++)
+            {
+                if (my_str[i] == to_del[0] && my_str[i + 1] == to_del[1])
+                {
+                    new_str += "C#";
+                    i++;
+                }
+                else
+                {
+                    new_str += my_str[i];
+                }
+            }
+            textBox2.Text = new_str;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();   
+            string my_str = textBox9.Text;
+            int c = my_str[0];
+            textBox2.Text = c.ToString();
+            textBox2.Text = c.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string my_str = textBox12.Text + " ";
+            int symb_count = 0;
+            for (int i = 0; i < my_str.Length - 1; i++)
+            {
+                if (my_str[i] != ' ' && my_str[i + 1] == ' ')
+                {
+                    symb_count++;
+                }
+            }
+            label1.Text = Convert.ToString(symb_count);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            string my_str = textBox13.Text + " ";
+            bool is_first = false;
+            string str = "";
+            for (int i = 0; i < my_str.Length - 1; i++)
+            {
+                if (my_str[i] != ' ' && my_str[i + 1] != ' ' && is_first != true)
+                {
+                    
+                    str += my_str[i];
+                }
+                else { is_first = true; }
+            }
+            textBox2.Text = str;
+        }
+    }
+}
