719818156b74e179420ac0014cca93234f54943a
Veler <43819843+BuTR3b@users.noreply.github.com>
2020-06-02 11:17:57 +0300

    Add files via upload

---
+++ Fraction/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fraction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myFraction fract1 = new myFraction();
+            textBox1.Text += fract1.ToString() + "\r\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myFraction fract2 = new myFraction(2);
+            textBox1.Text += fract2.ToString() + "\r\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myFraction fract3 = new myFraction(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            textBox1.Text += fract3.ToString() + "\r\n";
+        }
+    }
+}
---
+++ Fraction/myFraction.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class myFraction
+    {
+        private int numerator; // числит
+        private int denominator; // знаменат
+        public myFraction()
+        {
+            this.numerator = 0;
+            this.denominator = 1;
+        }
+
+        public myFraction(int num)
+        {
+            this.numerator = num;
+            this.denominator = num;
+        }
+
+        public myFraction(int num1, int num2)
+        {
+            this.numerator = num1;
+            this.denominator = num2;
+        }
+
+        public string ToString()
+        {
+            return this.numerator + "." + this.denominator;
+        }
+
+        public bool isTrueFraction()
+        {
+            if (this.numerator < this.denominator)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+
+        public int IntPart()
+        {
+            if (this.isTrueFraction())
+                return 0;
+            else
+            {
+                return this.numerator / this.denominator;
+            }
+        }
+
+        public myFraction Multi(myFraction other)
+        {
+            myFraction result = new myFraction(1, 1);
+            result.numerator = this.numerator * other.numerator;
+            result.denominator = this.denominator * other.denominator;
+            return result;
+        }
+
+        public myFraction Amount(myFraction other)
+        {
+            myFraction result = new myFraction(0, 0);
+            if (this.denominator == other.denominator)
+            {
+                result.numerator = this.numerator + other.numerator;
+                result.denominator = this.denominator;
+            }
+            if (this.denominator != other.denominator)
+            {
+                result.numerator = this.numerator * other.denominator + other.numerator * this.denominator;
+                result.denominator = this.denominator * other.denominator;
+            }
+            return result;
+        }
+        public static implicit operator double(myFraction a)
+        {
+            return (double)a.numerator / (double)a.denominator;
+        }
+    }
+}
