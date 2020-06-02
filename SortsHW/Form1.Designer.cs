b439b4798df698f696b63bbb172a88cbf0bd3fce
Veler <43819843+BuTR3b@users.noreply.github.com>
2020-06-02 11:34:49 +0300

    Add files via upload

---
+++ SortsHW/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SortsHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxM = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "M:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N:";
+            // 
+            // textBoxM
+            // 
+            this.textBoxM.Location = new System.Drawing.Point(39, 13);
+            this.textBoxM.Name = "textBoxM";
+            this.textBoxM.Size = new System.Drawing.Size(21, 20);
+            this.textBoxM.TabIndex = 2;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(39, 39);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(21, 20);
+            this.textBoxN.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 180);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 180);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вывести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Задание 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Задание 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 211);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxM;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SortsHW/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortsHW
+{
+    public partial class Form1 : Form
+    {
+        public int[,] array;
+        public int[,] sorted_array;
+        public int M;
+        public int N;
+        Random num = new Random();
+        List<int> my_list = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void swap(int num1, int num2)
+        {
+            int tmp = num1;
+            num1 = num2;
+            num2 = tmp;
+        }
+        List<int> bubble_sort_plus(List<int> list)
+        {
+            bool n = true;
+            while (n)
+            {
+                n = false;
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int tmp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = tmp;
+                        n = true;
+                    }
+                }
+            }
+            return list;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            M = Convert.ToInt32(textBoxM.Text);
+            N = Convert.ToInt32(textBoxN.Text);
+            array = new int[M, N];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = num.Next(1, 10);
+                    textBox1.Text += array[i, j] + " ";
+                }
+                textBox1.Text += "\r\n";
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            my_list.Clear();
+            sorted_array = new int[M, N];
+            if (M == N)
+            {
+                for (int i = 0; i < M; i++)
+                {
+                    for (int j = i; j >= N - i - 1; j--)
+                    {
+                        my_list.Add(array[i, j]);
+                    }
+                }
+                my_list = bubble_sort_plus(my_list);
+                int elem = my_list.Count - 1;
+                for (int i = 0; i < M; i++)
+                {
+                    for (int j = i; j >= N - i - 1; j--)
+                    {
+                        array[i, j] = my_list[elem];
+                        elem--;
+                    }
+                }
+                for (int i = 0; i < M; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        textBox2.Text += array[i, j] + " ";
+                    }
+                    textBox2.Text += "\r\n";
+                }
+            }
+            else
+            {
+                textBox2.Text += "МАТРИЦА НЕ КВАДРАТНОГО ВИДА";
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            int tmp;
+            int q;
+            int key;
+            int[] sum = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    sum[i] += array[j, i];
+                    textBox2.Text += array[j,i] + " ";
+                }
+                textBox2.Text += " = " + sum[i];
+                textBox2.Text += "\r\n";
+            }
+            for (int i = 1; i < N; i++)
+            {
+                if (sum[i] < sum[i - 1])
+                {
+                    tmp = sum[i];
+                    sum[i] = sum[i - 1];
+                    sum[i - 1] = tmp;
+                    for (int k = 0; k < M; k++)
+                    {
+                        tmp = array[k, i];
+                        array[k, i] = array[k, i - 1];
+                        array[k, i - 1] = tmp;
+                    }
+                }
+            }
+            /*for (int i = 0; i < N; i++)
+            {
+                q = i;
+                key = sum[i];
+                while (sum[q - 1] > key && q > 1) // выходит за границы
+                {
+                    tmp = sum[q - 1];
+                    sum[q - 1] = sum[q];
+                    sum[q] = tmp;
+                    q--;
+                }
+                sum[q] = key;
+            }*/
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    textBox2.Text += array[i,j] + " ";
+                }
+                textBox2.Text += "\r\n";
+            }
+        }
+    }
+}
